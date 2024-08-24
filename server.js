35eeaf91081f2981520ed8a36ccf6fde45df7b94
Simrankaur <kaursimran517404@gmail.com>
2024-08-24 19:53:36 +0530

    completed

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = 'https://whiteboard-five-jade.vercel.app';
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
