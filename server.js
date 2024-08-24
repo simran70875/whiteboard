d96a9a31a9a6ccab03924f508b4efcc1ff79bb06
Simrankaur <kaursimran517404@gmail.com>
2024-08-24 19:35:46 +0530

    done

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
